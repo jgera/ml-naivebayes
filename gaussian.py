c72346f20ead1ff0001c6ac98f7f8a98dc1191b8
PLR <plredmond@gmail.com>
2013-12-13 14:52:10 -0500

    removed optional arguments from model(..) and density(..) - made it clear whether var or std-dev is being passed; rewrote maths in density(..) - missing parens - use of math.pow & math.e changed to math.exp

--- gaussian.py
+++ gaussian.py
@@ -12,33 +12,35 @@ def mvue(fv, mu=None):
     return sum([math.pow(v - mu, 2) for v in fv]) / (len(fv) - 1)
 
 
-def density(mu, sd, stddev=True):
-    '''Return a Gaussian PDF with the given mean and variance or std deviation.
-
-    If stddev is true, treat sd as the standard deviation, otherwise treat it
-    as the variance.
-
+def density(mu, var):
+    '''float float -> (num -> float)
+    
+    Return a Gaussian PDF with the given mean and variance
+    
     '''
-    mu = float(mu)
-    var = math.pow(sd, 2) if stddev else sd
     if var == 0.0:
         raise ZeroVarianceError
-    return lambda x: math.pow(math.e, -1 * math.pow(x - mu, 2) / 2 * var) / math.sqrt(2 * math.pi * var)
+    # constants in the pdf
+    # mu = ..
+    # var = ..
+    sd = math.sqrt(var)
+    a = 1 / (sd * math.sqrt(2 * math.pi))
+    # ret
+    def pdf(x):
+        b = -1 * math.pow(x - mu, 2) / (2 * var)
+        return a * math.exp(b)
+    return pdf
 
 
-def model(fv, mu=None, sd=None):
-    '''Produce a Gaussian PDF for the feature vector.
-
-    mu is the estimated mean.
-    If the mu is None, use the mean of the feature values.
-
-    sd is the standard deviation.
-    If the sd is None, use MVUE to estimate variance.
+def model(fv):
+    '''[num] -> (num -> float)
+    
+    Produce a Gaussian PDF for the feature vector.
 
     '''
-    mu = mean(fv) if mu is None else mu
-    sd = mvue(fv, mu) if sd is None else sd
-    return density(mu, sd, not (sd is None))
+    mu = mean(fv)
+    var = mvue(fv)
+    return density(mu, var)
 
 class ZeroVarianceError(ValueError):
     pass
